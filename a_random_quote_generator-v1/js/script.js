b5bbf79d7b369265b2464cf63506639a73b45189
jilliandike <43615050+jilliandike@users.noreply.github.com>
2024-07-28 19:20:28 -0600

    Quotes array added and tested

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,8 +10,33 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+let quotes = [
+  {
+    quote: "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+    source: "Robert Frost",
+    citation: "The Road Not Taken by Robert Frost",
+    year: "1916"
+  },
+  {
+    quote: "Blessed is he who plants trees under whose shade he will never sit.",
+    source: "M. Trottier",
+    citation: "The Pall Mall Gazette",
+    year: "1868"
+  },
+  {
+    quote: "The two most important days in your life are the day you are born and the day you find out why.",
+    source: "Mark Twain"
+  },
+  {
+    quote: "Go confidently in the direction of your dreams.  Live the life you have imagined.",
+    source: "Henry David Thoreau"
+  },
+  {
+    quote: "If you want to lift yourself up, lift up someone else.",
+    source: "Booker T. Washington"
+  }
+];
+console.log(quotes);
 
 /***
  * `getRandomQuote` function
