300cf8589bdbcd2c2a86ad91eaf995e047bbdcde
jilliandike <43615050+jilliandike@users.noreply.github.com>
2024-07-29 10:41:54 -0600

    Added first and second functions
    First function is working correctly, second function is not.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,12 +10,13 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-let quotes = [
+const quotes = [
   {
     quote: "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
     source: "Robert Frost",
-    citation: "The Road Not Taken by Robert Frost",
-    year: "1916"
+    citation: "The Road Not Taken",
+    year: "1916",
+    about: "Robert Lee Frost was an American poet."
   },
   {
     quote: "Blessed is he who plants trees under whose shade he will never sit.",
@@ -25,15 +26,18 @@ let quotes = [
   },
   {
     quote: "The two most important days in your life are the day you are born and the day you find out why.",
-    source: "Mark Twain"
+    source: "Mark Twain",
+    about: "Samuel Langhorne Clemens, known by the pen name Mark Twain, was an American writer, humorist, and essayist."
   },
   {
     quote: "Go confidently in the direction of your dreams.  Live the life you have imagined.",
-    source: "Henry David Thoreau"
+    source: "Henry David Thoreau",
+    about: "Henry David Thoreau was an American naturalist, essayist, poet, and philosopher."
   },
   {
     quote: "If you want to lift yourself up, lift up someone else.",
-    source: "Booker T. Washington"
+    source: "Booker T. Washington",
+    about: "Booker Taliaferro Washington was an American educator, author, and orator."
   }
 ];
 console.log(quotes);
@@ -41,13 +45,62 @@ console.log(quotes);
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote() {
+  // 1. Create a variable that generates a random number
+  // between zero and the last index in the `quotes` array
+const randomNumber = Math.floor(Math.random() * 5);
+  
+// 2. Use the random number variable and bracket notation 
+  // to grab a random object from the `quotes` array, and 
+  // store it in a variable
+const getRandomNumber = quotes[randomNumber];
 
+  // 3. Return the variable storing the random quote object
+  console.log(getRandomNumber);
+  return getRandomNumber
+} getRandomQuote();
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  // 1. Create a variable that calls the getRandomQuote() 
+  // function
+const newQuote = getRandomQuote();
 
+  // 2. Create a variable that initiates your HTML string with 
+  // the first two <p></p> elements, their classNames, 
+  // and the quote and source properties, but leave off 
+  // the second closing `</p>` tag for now
+const html = `[
+  <p class= "quotes">randomQuote.quote
+  <p class= "source">randomQuote.source
+]`
+  // 3. Use an if statement to check if the citation property 
+  // exists, and if it does, concatenate a <span></span> 
+  // element, appropriate className, and citation property 
+  // to the HTML string
+  if (quotes.citation){
+    html += <span class='randomQuote.citation'></span>;
+  }
+
+  // 4. Use an if statement to check of the year property exists, 
+  // and if it does, concatenate a <span></span> element, 
+  // appropriate className, and year property to the HTML 
+  //string
+  if (quotes.year){
+    html += <span class='randomQuote.year'></span>;
+  };
+
+  // 5. After the two if statements, concatenate the closing </p> 
+  // tag to the HTML string
+html += `</p>.`
+
+  // 6. set the innerHTML of the quote-box div to equal the 
+  // complete HTML string
+  main.innerHTML += html;
+}
 
 
 /***
